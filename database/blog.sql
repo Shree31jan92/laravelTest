8bbc65bd0158f6c5e8e9aab496415d0a7b0c0943
unknown <mumhrtest@MUM042246.vistaprint.net>
2018-07-23 15:49:30 +0530

    database laravel test

--- database/blog.sql
+++ database/blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 23, 2018 at 11:23 AM
+-- Generation Time: Jul 23, 2018 at 12:18 PM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.10
 
@@ -54,8 +54,7 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`id`, `title`, `content`, `created_by`, `created_at`, `updated_at`) VALUES
-(2, 'Testing First Post', 'My first Post here', 1, '2018-07-23 03:42:16', '2018-07-23 03:42:16'),
-(3, 'kUNAL fIRST sTORY', 'kUNAL fIRST sTORYkUNAL fIRST sTORYkUNAL fIRST sTORYkUNAL fIRST sTORY', 2, '2018-07-23 03:46:09', '2018-07-23 03:46:09');
+(3, 'kUNAL fIRST sTORY edited by admin', 'kUNAL fIRST sTORYkUNAL fIRST sTORYkUNAL fIRST sTORYkUNAL fIRST sTORY', 2, '2018-07-23 03:46:09', '2018-07-23 04:43:23');
 
 -- --------------------------------------------------------
 
@@ -68,6 +67,7 @@ CREATE TABLE `users` (
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_type` int(11) NOT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -77,9 +77,10 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Shripad Mayekar', 'shree31jan92@gmail.com', '$2y$10$LZYCuw4oek3E6JptT96LxOGbbk3piR3UjV2TI9LcApfdwDF3ilaxS', 'bqav9XObjQyNT56yoqoox5TDTG8gwC5i0VGLcFDjSE48VLFeFWa6CJKDP4en', '2018-07-23 01:32:30', '2018-07-23 01:32:30'),
-(2, 'Kunal Mestri', 'kunal@gmail.com', '$2y$10$2272pbg69bIEMb4gBHGCpe.ypDN3qPayONWfrTiNuBGuIKhHQmWye', NULL, '2018-07-23 03:45:33', '2018-07-23 03:45:33');
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `user_type`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Shripad Mayekar', 'shree31jan92@gmail.com', '$2y$10$LZYCuw4oek3E6JptT96LxOGbbk3piR3UjV2TI9LcApfdwDF3ilaxS', 1, 'Pt2yzzwGjJxYVbVxGsr72BfmrySSyd6jtKvHFYJxbB1jplXuReeSVFdQ6Kii', '2018-07-23 01:32:30', '2018-07-23 01:32:30'),
+(2, 'Kunal Mestri', 'kunal@gmail.com', '$2y$10$2272pbg69bIEMb4gBHGCpe.ypDN3qPayONWfrTiNuBGuIKhHQmWye', 1, 'jv4ljMvTZN05NnrWZEtptduXGsm5W17kj0183jWJIxtK7qa6FEimu6GB5veK', '2018-07-23 03:45:33', '2018-07-23 03:45:33'),
+(3, 'Admin', 'admin@gmail.com', '$2y$10$7GBbh7GeCEPCvKNjCC2NDe/YNFONmWzf2CYZtC3rdfz2I.nyfoATa', 2, NULL, '2018-07-23 04:42:27', '2018-07-23 04:42:27');
 
 --
 -- Indexes for dumped tables
@@ -123,7 +124,7 @@ ALTER TABLE `posts`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
